1c8fd65fc03efcf6dc6eae89415e53811070a7a4
Chris Bailey <chris85bailey@aol.co.uk>
2021-03-24 13:50:50 +0000

    change nav styles, size of the header container, add some hover style and add  cursor pointer to all btn

--- dist/main.js
+++ dist/main.js
@@ -1,11 +1,32 @@
+const hamburger = document.querySelector('.hamburger');
+const hamburgerImg = document.querySelector('.hamburger img');
+const nav = document.querySelector('nav');
+const modelOpenBtns = document.querySelectorAll('.model-open-btn');
 const htmlHeight = document.querySelector('html').scrollHeight;
 const modelContainer = document.querySelector('.model-container');
 const thankYoumodelContainer = document.querySelector('.thank-you-model-container');
+const modelCloseBtn = document.querySelector('.model-close');
 
-console.log(htmlHeight)
 
+hamburger.addEventListener('click', () => {
+    nav.classList.contains('show') ? hamburgerImg.src = './images/icon-hamburger.svg' : hamburgerImg.src = './images/icon-close-menu.svg';
+    toggleShow(nav)
+})
+
+modelOpenBtns.forEach(btn => btn.addEventListener('click', () => {
+    toggleShow(modelContainer)
+}))
+
+modelCloseBtn.addEventListener('click', () => {
+    toggleShow(modelContainer)
+})
 
 setModelsHeight();
+
+
+function toggleShow(elm) {
+    elm.classList.toggle('show')
+}
 function setModelsHeight() {
     modelContainer.style.height = `${htmlHeight}px`
     thankYoumodelContainer.style.height = `${htmlHeight}px`
