11306325f095a69cecd87bd79fb7b887f5b450dc
Chris Bailey <chris85bailey@aol.co.uk>
2021-03-25 13:01:56 +0000

    added EventListener to btns, and radio btns, made icon svg for bookmarked, change size of thanks you model on 768px and up screens

--- dist/main.js
+++ dist/main.js
@@ -4,30 +4,74 @@ const nav = document.querySelector('nav');
 const modelOpenBtns = document.querySelectorAll('.model-open-btn');
 const htmlHeight = document.querySelector('html').scrollHeight;
 const modelContainer = document.querySelector('.model-container');
+const modelCards = document.querySelectorAll('.model-container .card');
+const modelContainerBtn = document.querySelectorAll('.pledge-btn');
 const thankYoumodelContainer = document.querySelector('.thank-you-model-container');
 const modelCloseBtn = document.querySelector('.model-close');
+const thankYouModelBtn = document.querySelector('.thank-you-btn');
+const bookmarkBtn = document.querySelector('.bookmark');
+const bookmarkBtnSpan = document.querySelector('.bookmark span');
+const bookmarkBtnImg = document.querySelector('.bookmark-img');
+const radioBtns = document.querySelectorAll('.radio');
+const pledgeContainer = document.querySelectorAll('.pledge');
 
 
 hamburger.addEventListener('click', () => {
     nav.classList.contains('show') ? hamburgerImg.src = './images/icon-hamburger.svg' : hamburgerImg.src = './images/icon-close-menu.svg';
-    toggleShow(nav)
-})
+    toggleShow(nav);
+});
 
-modelOpenBtns.forEach(btn => btn.addEventListener('click', () => {
-    toggleShow(modelContainer)
-}))
+modelOpenBtns.forEach((btn, idx) => btn.addEventListener('click', () => {
+    radioBtns[idx].checked = true;
+    if (radioBtns[idx].checked) {
+        pledgeContainer[idx].classList.add('show');
+        modelCards[idx].style.borderColor = '#3cb4ac';
+    }
+
+    toggleShow(modelContainer);
+}));
 
 modelCloseBtn.addEventListener('click', () => {
-    toggleShow(modelContainer)
+    toggleShow(modelContainer);
+});
+radioBtns.forEach((btn, idx) => btn.addEventListener('click', () => {
+    radioBtns.forEach(btn => btn.checked = false);
+    pledgeContainer.forEach(container => container.classList.remove('show'));
+    modelCards.forEach(card => card.style.borderColor = 'rgba(122, 122, 122, 0.4)');
+    btn.checked = true;
+    if (btn.checked) {
+        pledgeContainer[idx].classList.add('show');
+        modelCards[idx].style.borderColor = '#3cb4ac';
+    }
+}))
+modelContainerBtn.forEach(btn => btn.addEventListener('click', () => {
+    toggleShow(modelContainer);
+    toggleShow(thankYoumodelContainer);
+}));
+
+thankYouModelBtn.addEventListener('click', () => {
+    toggleShow(thankYoumodelContainer);
+});
+
+
+bookmarkBtn.addEventListener('click', () => {
+    bookmarkBtn.classList.toggle('bookmarked');
+    if (bookmarkBtn.classList.contains('bookmarked')) {
+        bookmarkBtnImg.src = './images/icon-bookmarked.svg';
+        bookmarkBtnSpan.textContent = 'Bookmarked';
+    } else {
+        bookmarkBtnImg.src = './images/icon-bookmark.svg';
+        bookmarkBtnSpan.textContent = 'Bookmark';
+    }
 })
 
 setModelsHeight();
 
 
 function toggleShow(elm) {
-    elm.classList.toggle('show')
+    elm.classList.toggle('show');
 }
 function setModelsHeight() {
-    modelContainer.style.height = `${htmlHeight}px`
-    thankYoumodelContainer.style.height = `${htmlHeight}px`
+    modelContainer.style.height = `${htmlHeight}px`;
+    thankYoumodelContainer.style.height = `${htmlHeight}px`;
 }
